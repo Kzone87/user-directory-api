0f35b1299ebca9a76617e8f06ae071dca2d49593
Kzone87 <168076046+Kzone87@users.noreply.github.com>
2026-09-05 13:57:47 +0900

    add work order state transition error

---
+++ src/main/java/dev/kzone/portfolio/userapi/exception/WorkOrderTransitionException.java
@@ -0,0 +1,7 @@
+package dev.kzone.portfolio.userapi.exception;
+
+public class WorkOrderTransitionException extends RuntimeException {
+    public WorkOrderTransitionException(String message) {
+        super(message);
+    }
+}
